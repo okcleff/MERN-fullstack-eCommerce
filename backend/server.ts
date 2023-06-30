a702829c8b0bd7d8e52f9fccd2f14259135fd014
okcleff <okcleff@gmail.com>
2023-06-30 18:17:25 +0900

    backend 초기 세팅

---
+++ backend/server.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import { PRODUCTS } from "./data/products";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, res) => res.send("API is running..."));
+
+app.get("/api/products", (_, res) => res.json(PRODUCTS));
+
+app.get("/api/products/:productsId", (req, res) => {
+  const product = PRODUCTS.find((p) => p._id === req.params.productsId);
+  res.json(product);
+});
+
+app.listen(port, async () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
