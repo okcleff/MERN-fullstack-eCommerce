f1f2764097dc48442779e675ab2d07eb5b8b2c44
okcleff <okcleff@gmail.com>
2023-06-30 11:05:46 +0900

    커스텀 부트스트랩 테마 적용, 헤더, 푸터 컴포넌트 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-  return <h1>Welcome to OK mall!</h1>;
+  return (
+    <>
+      <Header />
+
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to OKmall!</h1>
+        </Container>
+      </main>
+
+      <Footer />
+    </>
+  );
 }
 
 export default App;
